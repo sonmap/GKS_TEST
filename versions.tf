0ad4d0301a2786845f7dfc7dae93d7b1ed684442
map son <sohnkyoungwon@gmail.com>
2021-12-20 13:04:11 +0900

    second3233ddd

--- versions.tf
+++ versions.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.5.0"
+      version = "3.52.0"
       
     }
     
-    
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 1.6.0"
+      version = ">= 2.0.0"
      
     }
     
